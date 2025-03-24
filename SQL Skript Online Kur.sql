eb27686dde6d20db25cf8ff789fe32c6e672c402
TLSenz <137443590+TLSenz@users.noreply.github.com>
2025-03-24 08:55:36 +0100

    Update SQL Skript Online Kur.sql
    added not nul constraint to every attribute

--- SQL Skript Online Kur.sql
+++ SQL Skript Online Kur.sql
@@ -9,21 +9,21 @@ CREATE TABLE Kategorien (
 
 CREATE TABLE Platform(
     id int Auto_increment Primary Key,
-    name VARCHAR(255)
+    name VARCHAR(255) not null
 );
 
 create TABLE Ort(
     id int Auto_Increment Primary Key,
-    name varchar(255),
-    PLZ int
+    name varchar(255) not null,
+    PLZ int not null
 );
 create Table Trainer
 (
     id         int Auto_Increment Primary Key,
-    Geburtstag Date,
-    Bio        text,
-    Ort_id     int,
-    Email      varchar(255),
+    Geburtstag Date not null,
+    Bio        text not null,
+    Ort_id     int not null,
+    Email      varchar(255) not null,
     foreign key (Ort_ID) references Ort (id)
 );
 
@@ -33,20 +33,20 @@ CREATE TABLE Kurse (
     titel VARCHAR(255) NOT NULL,
     datum DATE NOT NULL,
     dauer INT NOT NULL,
-    schwierigkeit int,
-    kategorie_id INT,
-    Trainer_id int,
-    Platform_id int,
+    schwierigkeit int not null,
+    kategorie_id INT not null,
+    Trainer_id int not null,
+    Platform_id int not null,
     FOREIGN KEY (Platform_id) REFERENCES Platform(id),
     FOREIGN KEY (Trainer_id) REFERENCES Trainer(id),
-    FOREIGN KEY (kategorie_id) REFERENCES Kategorien(id) ON DELETE SET NULL
+  
 
 );
 
 CREATE TABLE Kategorie_Kurs(
     id int Auto_Increment Primary Key,
-    kategorie_id int,
-    Kurs_id int,
+    kategorie_id int not null,
+    Kurs_id int not null,
     Foreign key (kategorie_id) references Kategorien(id),
     foreign key (Kurs_id) references Kurse(id)
 );
@@ -55,19 +55,19 @@ CREATE TABLE Kategorie_Kurs(
 CREATE TABLE Portalbenutzer(
     id INT AUTO_INCREMENT PRIMARY KEY,
     Benutzername VARCHAR(255) NOT NULL,
-    email varchar(255),
-    Telefonnummer int,
-    Geburtsdatum date,
+    email varchar(255) not null,
+    Telefonnummer int not null,
+    Geburtsdatum date not null,
     Passwort varchar(255)
 
 );
 
 -- Tabelle: Teilnahme (Viele-zu-Viele Beziehung zwischen Kurse und Teilnehmer)
 CREATE TABLE Teilnahme (
-    kurs_id INT,
-    teilnehmer_id INT,
-    bewertung INT CHECK (bewertung BETWEEN 1 AND 5),
-    kommentar TEXT,
+    kurs_id INT not null,
+    teilnehmer_id INT not null,
+    bewertung INT CHECK (bewertung BETWEEN 1 AND 5) not null,
+    kommentar varchar(255) not null,
     PRIMARY KEY (kurs_id, teilnehmer_id),
     FOREIGN KEY (kurs_id) REFERENCES Kurse(id) ON DELETE CASCADE,
     FOREIGN KEY (teilnehmer_id) REFERENCES Portalbenutzer(id) ON DELETE CASCADE
